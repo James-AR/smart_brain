40816409bdb159148b630f281cc47ccf4b99e8d2
James <ruiz.james12@yahoo.com>
2019-08-27 15:20:21 -0700

    Face recognition API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Particles from 'react-particles-js'
 import Navigation from './components/Navigation/Navigation'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+import Clarifai from 'clarifai'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
+
+const app = new Clarifai.App({
+  apiKey: '4b525e57969e4c609c6bdf275ef23dd6'
+ });
 
 const particlesOptions = {
   particles: {
@@ -18,19 +24,49 @@ const particlesOptions = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles'
-          params={particlesOptions}
-        />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: '',
+      imageURL: ''
+    }
+  }
+
+  onInputChange = event => {
+    this.setState({input: event.target.value})
+  }
+
+  onSubmit = () => {
+    this.setState({imageURL: this.state.input})
+    console.log('Clicked')
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL, 
+      this.state.input)
+      .then(
+    function(response) {
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
+    },
+    function(err) {
+      // there was an error
+    }
   );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+            params={particlesOptions}
+          />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onSubmit}/>
+        <FaceRecognition imageURL={this.state.imageURL} />
+      </div>
+    );
+  }
 }
 
 export default App;
